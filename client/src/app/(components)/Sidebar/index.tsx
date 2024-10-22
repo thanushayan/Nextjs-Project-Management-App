b0cad55c60d77e71a1d23bc1eef463756c49260d
thanushayan <thanushayan17@gmail.com>
2024-10-22 21:27:23 +0530

    nav bar

---
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React, { useState } from 'react'
+ 
+
+const Sidebar = ( ) => {
+    const [showProject,setShowProject] = useState(true);
+    const [showPriority,setShowPriority] = useState(true);
+
+    const sidebarClassNames = `fixed flex  flex-col h-[100%] justify-between shadow-xl
+    transition-all duration-300 h-full z-40 dark:bg-black overflow-y-auto bg`
+
+  return   <div>index</div>
+   
+ 
+}
+
+export default Sidebar;
