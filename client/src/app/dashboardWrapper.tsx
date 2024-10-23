eadbcde2a2091c66a6214bbe11a8eeff26e1d879
thanushayan <thanushayan17@gmail.com>
2024-10-23 10:59:25 +0530

    context provider

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,10 +1,24 @@
- import React from 'react'
+ "use client"
+ 
+ import React, { useEffect } from 'react'
  import Navbar from "@/app/(components)/Navbar";
  import Sidebar from  "@/app/(components)/Sidebar";
+import StoreProvider, { useAppSelector } from './redux';
  
  
  
- const DashboardWrapper = ({children}: {children: React.ReactNode} ) => {
+ const DashboardLayout = ({children}: {children: React.ReactNode} ) => {
+  const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed,
+);
+const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+useEffect(() => {
+  if(isDarkMode) {
+    document.documentElement.classList.add("dark")
+
+  }
+})
+  
    return (
      <div className="flex min=h-screen w-full bg-gray-50 text-gray-900 "> 
     {/*sidebar */}
@@ -19,6 +33,14 @@
 
      </div>
    )
+ }
+ const DashboardWrapper = ({children } : {children : React.ReactNode }) =>  {
+  return (
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
+
+  )
  }
  
  export default DashboardWrapper
--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,3 +1,5 @@
+ 
+
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
