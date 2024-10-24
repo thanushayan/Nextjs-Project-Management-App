22b2941842df03925f43187e9d83eb5aa753c7b6
thanushayan <thanushayan17@gmail.com>
2024-10-24 11:11:31 +0530

    backend server folder setup and  prisma installation

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -1,7 +1,24 @@
 "use client";
 import Image from "next/image";
 import React, { useState } from 'react'
-import {  LockIcon, LucideIcon,Home,Briefcase,Search,Settings,User,Users,X, ChevronUp, ChevronDown} from "lucide-react";
+import { 
+   LockIcon,
+   LucideIcon,
+   Home,
+   Briefcase,
+   Search,
+   Settings,
+   User,
+   Users,
+   X,
+   ChevronUp,
+   ChevronDown,
+   AlertCircle,
+   ShieldAlert,
+   AlertTriangle,
+   AlertOctagon,
+   Layers3} from "lucide-react";
+
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useAppDispatch, useAppSelector } from "@/app/redux";
@@ -61,6 +78,7 @@ const Sidebar = ( ) => {
           <SidebarLink icon={User} label="Users" href="/users" />
           <SidebarLink icon={Users} label="Teams" href="/teams" />
       </nav>
+      {/* projects link  */}
       <button onClick={() => setShowProjects((prev) => !prev)}
       className="flex w-full items-center justify-between px-8 py-3 text-gray-500">
         <span className="">Projects</span>
@@ -70,6 +88,26 @@ const Sidebar = ( ) => {
       </button>
 
       {/* project list  */}
+      {/* priorities links  */}
+
+      <button onClick={() => setShowPriority((prev) => !prev)}
+      className="flex w-full items-center justify-between px-8 py-3 text-gray-500">
+        <span className="">Priority</span>
+        {showPriority ? (
+          <ChevronUp className="h-5 w-5" />
+        ) : <ChevronDown className="h-5 w-5"/>}
+      </button>
+
+    {showPriority && (
+      <>
+      <SidebarLink icon={AlertCircle} label="Urgent" href="/priority/urgent"/>
+      <SidebarLink icon={ShieldAlert} label="High" href="/priority/high"/>
+      <SidebarLink icon={AlertTriangle} label="Medium" href="/priority/medium"/>
+      <SidebarLink icon={AlertOctagon} label="Low" href="/priority/low"/>
+      <SidebarLink icon={Layers3} label="Backlog" href="/priority/backlog"/>
+      
+      </>
+    )}
 
 
       
