a4e859ec4c693f9d689772b59eb334798c72588d
thanushayan <thanushayan17@gmail.com>
2024-10-25 14:28:14 +0530

    api task

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -63,13 +63,26 @@ export interface Task {
 export const api = createApi({
     baseQuery: fetchBaseQuery({baseUrl:process.env.NEXT_PUBLIC_API_BASE_URL}),
     reducerPath: "api",
-    tagTypes:["Projects"],
+    tagTypes:["Projects","Tags"],
     endpoints:(build) => ({
         getProjects:build.query<Project[],void>({
             query: () => "projects",
             providesTags : ["Projects"]
-        })
+        }),
+        createProject: build.mutation<Project, Partial<Project>>({
+            query:(project) => ({
+                url:"projects",
+                method:"POST",
+                body: project
+            }),
+            invalidatesTags: ["Projects"],
+        }),
+        getTasks:build.query<Task[],{projectId: number}>({
+            query: (projectId) => `tasks?projectId =${projectId}`,
+            providesTags :  (result) => result ? result.map(({id}) => ({type : "Tasks" as const, id })): [{type : "Tasks" as const}],
+        }),
     }),
 })
 
-export const {} = api ;
+
+export const {useGetProjectsQuery,useCreateProjectMutation} = api ;
