260b3150370696f2d7cf9c8e1760b5416778199e
thanushayan <thanushayan17@gmail.com>
2024-11-07 10:51:29 +0530

    taskcard

--- client/src/app/projects/ListView/index.tsx
+++ client/src/app/projects/ListView/index.tsx
@@ -1,4 +1,7 @@
-import { useGetTasksQuery } from '@/state/api';
+import Header from "@/components/Header";
+import TaskCard from "@/app/components/TaskCard";
+import { useGetTasksQuery,Task } from '@/state/api';
+// import { Task } from "gantt-task-react";
 import React from 'react'
 
 type Props = {
@@ -13,7 +16,13 @@ const index = ({ id, setIsModalNewTaskOpen }: Props) => {
 
   return  <div className="px-4 pb-8 xl:px-6 ">
     <div className="pt-5">
-        
+        <Header name = "List"/>
+
+    </div>
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 lg:gap-6 ">
+        {tasks?.map((task:Task)=><TaskCard key={task.id} task={task}/>
+
+        )}
     </div>
     
     </div>
---
+++ client/src/components/TaskCard/index.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import {format} from "date-fns";
+import {Task} from "@/state/api"
+import Image from 'next/image';
+
+type Props = {
+    task:Task
+}
+
+const index = ({task}: Props) => {
+  return <div className="mb-3 rounded bg-white p-4 shadow dark:bg-dark-secondary dark:text-white">
+    {task.attachments && task.attachments.length> 0 && (
+        <div>
+            <strong>Attachments:</strong>
+            <div className="flex flex-wrap">
+                {task.attachments && task.attachments.length> 0 && (
+                      <Image
+                      
+                      src={`/${task.attachments[0].fileURL}`}
+                      alt={task.attachments[0].fileName}
+                      width={400}
+                      height={200}
+                      className="rounded-md"
+                    />
+                )}
+                </div>
+        </div>
+
+    ) }
+    <p>
+    <strong>ID:</strong> {task.id}
+    </p>
+    <p>
+    <strong>Title:</strong> {task.title}
+    </p>
+    <p>
+    <strong>Description:</strong> {""}
+    {task.description || "No description provided "}
+    </p>
+    <p>
+    <strong>Status:</strong> {task.status}
+    </p>
+    <p>
+    <strong>Priority:</strong> {task.priority}
+    </p>
+    <p>
+    <strong>Tags:</strong> {task.tags || "No tags"}
+    </p>
+    <p>
+    <strong>Start Date :</strong> {""}
+    {task.startDate ? format(new Date(task.startDate), "p") : "Not set "}
+    </p>
+    <p>
+    <strong>Start Date :</strong> {""}
+    {task.startDate ? format(new Date(task.startDate), "p") : "Not set "}
+    </p>
+    <p>
+    <strong>ID:</strong> {task.id}
+    </p>
+    
+    </div>
+    
+  
+}
+
+export default index
