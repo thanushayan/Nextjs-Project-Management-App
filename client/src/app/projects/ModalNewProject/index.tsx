05df70a30770062b8a398e87d63c67bd9a84d067
thanushayan <thanushayan17@gmail.com>
2024-11-08 23:26:06 +0530

    modalnewproject pop up

--- client/src/app/projects/ModalNewProject/index.tsx
+++ client/src/app/projects/ModalNewProject/index.tsx
@@ -1,3 +1,4 @@
+import Modal from '@/components/Modal';
 import { useCreateProjectMutation } from '@/state/api';
 import React, { useState } from 'react'
 
@@ -23,13 +24,65 @@ const ModalNewProject = ({isOpen,onClose}: Props) => {
             description,
             startDate,
             endDate
-        })
+        });
 
-    }
+    };
+    const isFormValid = () => {
+      return projectName && description && startDate && endDate;
 
-  return (
-    <div>ModalNewProject</div>
-  )
+    };
+
+    const inputStyles = "w-full rounded border border-gray-300 p-2 shadow-sm dark:border-dark-tertiary  dark:bg-dark-tertiary dark:text-white dark:focus:outline-none";
+
+  return  <Modal isOpen={isOpen} onClose={onClose} name="Create New Project ">
+    <form 
+    className="mt-4 space-y-6 "
+    onSubmit={(e) => {
+      e.preventDefault();
+      handleSubmit();
+    }}
+    >
+      <input type="text" 
+      className={inputStyles}
+       placeholder="Project Name" 
+       value={projectName}
+        onChange={(e) => setProjectName(e.target.value)}
+      />
+       <textarea
+          className={inputStyles}
+          placeholder="Description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 sm:gap-2 ">
+        <input type="date" 
+       className={inputStyles} 
+       value={startDate}
+        onChange={(e) => setStartDate(e.target.value)}
+      />
+        <input type="date" 
+      className={inputStyles}
+       value={endDate}
+        onChange={(e) => setEndDate(e.target.value)}
+      />
+      </div>
+         <button
+         type="submit"
+         className={`mt-4 flex w-full justify-center rounded-md border border-transparent bg-blue-primary px-4 py-2 
+         text-base font-medium text-white shadow-sm  hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-600
+         focus-offset-2 
+         ${!isFormValid() || isLoading ? "cursor-not-allowed opacity-50 " : ""
+         }`}
+         disabled={!isFormValid() || isLoading}
+         >
+          {isLoading ? "Creating..." : "Create Project "}
+
+         </button>
+
+    </form>
+  </Modal>;
+   
+   
 }
 
 export  default ModalNewProject
