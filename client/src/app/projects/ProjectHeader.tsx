9014c1f9d8322dcfd4d9f01cb3f4aed0c94ec7f6
thanushayan <thanushayan17@gmail.com>
2024-11-08 18:19:54 +0530

    Modal and header

--- client/src/app/projects/ProjectHeader.tsx
+++ client/src/app/projects/ProjectHeader.tsx
@@ -1,5 +1,5 @@
 import Header from '@/components/Header';
-import { Clock, Filter, Grid3X3, List, Share2, Table } from 'lucide-react';
+import { Clock, Filter, Grid3X3, List, Share2, Table,PlusSquare } from 'lucide-react';
 import React, { useState } from 'react';
 
 type Props = {
@@ -8,14 +8,25 @@ type Props = {
 };
 
 const ProjectHeader = ({ activeTab, setActiveTab }: Props) => {
-  const [isModalNewProjectOpen, setIsModalProjectOpen] = useState(false);
+  const [isModalNewProjectOpen, setIsModalNewProjectOpen] = useState(false);
  
 
   return  <div className="px-4 xl:px-6">
       
-      {/* Modal new project */}
+      {/* <ModalNewProject 
+      isOpen={isModalNewProjectOpen}
+      onClose={() => setIsModalNewProjectOpen(false)}
+      /> */}
       <div className="pb-6 pt-6 lg:pb-4 lg:pt-8">
-        <Header name="Product Design Development" />
+        <Header name="Product Design Development" 
+        buttonComponent={
+          <button
+          className="flex items-center rounded-md bg-blue-primary px-3 py-2 text-white hover:bg-blue-600"
+          onClick={()=> setIsModalNewProjectOpen(true)}
+          >
+            <PlusSquare className="mr-2 h-5 w-5"/> New Boards
+          </button>
+        }/>
       </div>
 
       {/* Tabs */}
